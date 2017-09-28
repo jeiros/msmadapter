e90f8f2acc3ed97872731bce60d57731d342bf77
Juan Eiros <jeirosz@gmail.com>
2017-09-28 10:29:37 +0100

    renames

--- msmadapter/test.py
+++ msmadapter/test.py
@@ -1,5 +1,5 @@
-from msmadapter.Simulation import App, Adaptive
+from msmadapter.simulation import App, Adaptive
 app = App(meta='meta.pandas.pickl')
 ad = Adaptive(app=app, stride=20)
 ad.fit_model()
-ad.find_respawn_fame()
+ad.find_respawn_conformations()
