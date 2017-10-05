f5fb528a33addbf2d4adfce7d4cce1825e2e7e92
Juan Eiros <jeirosz@gmail.com>
2017-10-05 12:04:45 +0100

    Revert "add requirements"
    This reverts commit ed95a2fcb79eed8498242484c3092676908323cd.

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt', 'r') as f:
-    requirements = f.read()
-
-requirements = list(filter(None, requirements.split('\n')))
-
 setup(
     name='MSMadapter',
     version='0.1',
@@ -13,8 +8,7 @@ setup(
     author='Juan Eiros',
     author_email='jeiroz@gmail.com',
     package_data={
-        'MSMadapter' : ['README.md', 'requirements.txt', 'templates/*']
+        'MSMadapter' : ['README.md', 'requirements.txt']
     },
-    install_requires=requirements,
     include_package_data=True
 )
