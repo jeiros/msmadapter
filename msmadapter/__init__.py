22a8db077d74a6326e8676db7066053995928144
Juan Eiros <j.eiros-zamora14@imperial.ac.uk>
2017-11-06 14:40:05 +0000

    new logger config

--- msmadapter/__init__.py
+++ msmadapter/__init__.py
@@ -1,3 +1,18 @@
+# Taken from https://gist.github.com/st4lk/6287746
 import logging
-logging.basicConfig(level=logging.DEBUG)
-logging.getLogger(__name__).addHandler(logging.NullHandler())
+import logging.handlers
+
+f = logging.Formatter(fmt='%(asctime)s %(levelname)s:%(name)s: %(message)s '
+    '(%(filename)s:%(lineno)d)',
+    datefmt="%Y-%m-%d %H:%M:%S")
+handlers = [
+    logging.handlers.RotatingFileHandler('rotated.log', encoding='utf8',
+        maxBytes=100000, backupCount=1),
+    logging.StreamHandler()
+]
+root_logger = logging.getLogger()
+root_logger.setLevel(logging.DEBUG)
+for h in handlers:
+    h.setFormatter(f)
+    h.setLevel(logging.DEBUG)
+    root_logger.addHandler(h)
