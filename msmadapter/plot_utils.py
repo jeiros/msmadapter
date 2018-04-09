9d22882365cae0a7337c2941b8a08c007c073e3d
Juan Eiros <j.eiros-zamora14@imperial.ac.uk>
2018-04-09 15:58:16 +0100

    plots update

--- msmadapter/plot_utils.py
+++ msmadapter/plot_utils.py
@@ -21,6 +21,15 @@ def plot_spawns(inds, tica_trajs, ax=None, obs=(0, 1), color='red', base_size=10
 
 
 def plot_tica_landscape(tica_trajs, ax=None, figsize=(7, 5), obs=(0, 1), cmap='magma'):
+    """
+    Plots a 2D landscape of the tica trajectories
+    :param tica_trajs: dictionary of tica trajectories
+    :param ax: mpl.axies
+    :param figsize: tuple
+    :param obs: tuple
+    :param cmap: str
+    :return: f, ax (figure and axis)
+    """
     if ax is None:
         f, ax = pp.subplots(figsize=figsize)
 
@@ -35,13 +44,28 @@ def plot_tica_landscape(tica_trajs, ax=None, figsize=(7, 5), obs=(0, 1), cmap='m
     return f, ax
 
 
-def plot_clusters(clusterer, ax=None, obs=(0, 1), base_size=50,
+def plot_clusters(clusterer, ax=None, selection=None, obs=(0, 1), base_size=50,
                   alpha=0.5, color='yellow'):
+    """
+    Plots the position of clusters in a two dimensional landscape
+    :param clusterer: a clusterer object from msmbuilder
+    :param ax: mpl.axis
+    :param selection: None or list of ints, which clusters to plot
+    :param obs: tuple
+    :param base_size: int
+    :param alpha: float
+    :param color: str
+    :return: ax
+    """
+
     if ax is None:
         ax = pp.gca()
 
-    prune = clusterer.cluster_centers_[:, obs]
-    scale = clusterer.counts_ / numpy.max(clusterer.counts_)
+    if selection is None:
+        prune = clusterer.cluster_centers_[:, obs]
+    else:
+        prune = clusterer.cluster_centers_[selection][:, obs]
+    scale = clusterer.counts_ + 1 / numpy.max(clusterer.counts_)  # Add one to still plot clusters with no counts
 
     ax.scatter(
         prune[:, 0],
